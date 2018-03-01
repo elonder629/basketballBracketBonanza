6bc83740dcb58e9ec14fd6b33d1cf6e15ff7dbcf
jd997 <john.m.durrell@gmail.com>
2018-03-01 08:38:07 -0500

    temp

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,26 @@
 package com.company;
 
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("LETS GET READY TO RUMBLE");
+        Scanner scanner = new Scanner(new File("RegularSeasonCompactResults.tsv"));
+        ArrayList<Team> teams = new ArrayList<Team>();
+        boolean flag = false;
+        while(scanner.hasNext()) {
+            if(!flag) {
+                flag = true;
+                continue;
+            }
+            String line = scanner.nextLine();
+            Scanner scanner2 = new Scanner(line);
+            if (scanner2.next().equals("2017")) {
+                break;
+            }
+            scanner2.next();
 
+        }
     }
 }
