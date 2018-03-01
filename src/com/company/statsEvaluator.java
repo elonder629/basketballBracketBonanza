e375a69c245d12c5e13563e94cd20bcc272b8b89
jd997 <john.m.durrell@gmail.com>
2018-03-01 08:17:57 -0500

    fixed text file

--- src/com/company/statsEvaluator.java
+++ src/com/company/statsEvaluator.java
@@ -4,6 +4,7 @@ package com.company;
  * Created by el693 on 2/28/18.
  */
 public class statsEvaluator {
+
     public statsEvaluator(){
 
     }
@@ -11,8 +12,5 @@ public class statsEvaluator {
     public double pythagoreanPercentageCalc(int pointsFor, int pointsAgainst, int gamesPlayed){
         return gamesPlayed*(Math.pow(pointsFor,16.5)/(Math.pow(pointsFor,16.5) + Math.pow(pointsAgainst,16.5)));
     }
-
-    public
-
 
 }
