8e0bfa9cc6d52fbf7af8220a85a8ad93b46272e5
kd944 <deleon_k@ymail.com>
2018-03-01 08:50:06 -0500

    uuu

--- src/com/company/statsEvaluator.java
+++ src/com/company/statsEvaluator.java
@@ -13,4 +13,9 @@ public class statsEvaluator {
         return gamesPlayed*(Math.pow(pointsFor,16.5)/(Math.pow(pointsFor,16.5) + Math.pow(pointsAgainst,16.5)));
     }
 
+    public double fantasyBasketball(int WFGM, int WFGM3, int WFTM, int WOR, int WDR, int WAst, int WStl, int WBlk, int WTO){
+        double sum = (WFGM * 2) + (WFGM3 * 3) + WFTM + ((WOR + WDR) * 1.2) + (WAst * 1.5) + (WStl * 3) + (WBlk * 3) + (WTO * -1);
+        return sum;
+    }
+
 }
