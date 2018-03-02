72ba482a774db9d142e0a080fc6ecc9afc643a49
elonder629 <ericlonder730@gmail.com>
2018-03-02 08:12:21 -0500

    gamescore is finished

--- src/com/company/statsEvaluator.java
+++ src/com/company/statsEvaluator.java
@@ -12,7 +12,9 @@ public class statsEvaluator {
     public static double gameScoreCalc(int points, int fieldGoals
             , int FGA, int FT, int FTA, int ORB, int DRB, int steals
             , int assists, int blocks, int personalFouls, int turnovers){
-        return points +(0.4*fieldGoals)- (0.7*FGA) - (0.4*(FTA - FT)) + (0.7 * ORB) + ()
+        return points +(0.4*fieldGoals)- (0.7*FGA) - (0.4*(FTA - FT))
+                + (0.7 * ORB) + (0.3 * DRB) + steals + (0.7 * assists)
+                + (0.7 * blocks) - (0.4 * personalFouls) - turnovers;
     }
 
     public double fantasyBasketball(int WFGM, int WFGM3, int WFTM, int WOR, int WDR, int WAst, int WStl, int WBlk, int WTO){
