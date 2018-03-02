cec73540bdb5252eccf19021cc988fd8d649d6dd
Veronica Lang <TheSlug64@gmail.com>
2018-03-02 14:07:58 -0500

    Update statsEvaluator.java

--- src/com/company/statsEvaluator.java
+++ src/com/company/statsEvaluator.java
@@ -21,5 +21,30 @@ public class statsEvaluator {
         double sum = (WFGM * 2) + (WFGM3 * 3) + WFTM + ((WOR + WDR) * 1.2) + (WAst * 1.5) + (WStl * 3) + (WBlk * 3) + (WTO * -1);
         return sum;
     }
+    
+    
+    
+    //u can look over/check this wip to see if it's garbage or not tthank 
+        static double eloDiff;
+    public double findEloRating(){
+        double rating = 0; //change elodiff to whatever is needed to determine elo ratings.
+        //add the rating to an array
+        return rating;
+    }
+    public double eloDifference (String teamA, String teamB){ //find values determined from findElorating from team names
+
+        //double eloDifference = (arrayname).get(whatever index the elorating is in)
+        double elodiff = 0; //change the elodiff to equal to team A's elo rating minus team B's Elo rating
+        return elodiff;
+    }
+    public double Elo(){
+        //calculate teams's probability of winning: Pr(A) = 1/10^(-eloDiff/400)+1)
+        //elodiff is team A's elo rating minus Team B's Elo Rating
+        //so like we predict all of them of who will win out of them all (let's think abuot the number of teams later
+        //make an arraylist to find the top winner in the whole list of names and then remove that name (clear) after
+        // it's determined
+        double determineBestTeam = 1/Math.pow(10,-eloDiff+1/400);
+        return Elo(); //change
+    }
 
 }
