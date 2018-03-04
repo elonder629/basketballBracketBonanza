af2ca3fb862e1037d1814c89af17ea3a8f91e4eb
John <john.m.durrell@gmail.com>
2018-03-04 15:26:40 -0500

    implemented team names

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("LETS GET READY TO RUMBLE");
         Scanner scanner = new Scanner(new File("RegularSeasonCompactResults.tsv"));
+        Scanner scannerT = new Scanner(new File("Teams.tsv"));
         boolean flag = true;
+        while(scannerT.hasNext()) {
+            if (flag) {
+                scannerT.nextLine();
+                flag = false;
+                continue;
+            }
+            String line = scannerT.nextLine();
+            Scanner scannerT2 = new Scanner(line);
+            scannerT2.useDelimiter("\t");
+            String id = scannerT2.next();
+            String name = scannerT2.next();
+            scannerT2.close();
+            teams.add(new Team(name, id));
+            teamIDs.add(id);
+        }
+        scannerT.close();
+        flag = true;
         while(scanner.hasNextLine()) {
             if (flag) {
                 scanner.nextLine();
@@ -20,7 +38,7 @@ public class Main {
             String line = scanner.nextLine();
             Scanner scanner2 = new Scanner(line);
             scanner2.useDelimiter("\t");
-            if (scanner2.next().equals("2017")) {
+            if (scanner2.next().equals("2017")) { //if only testing 2016
                 break;
             }
             scanner2.next();
@@ -29,29 +47,31 @@ public class Main {
             String losingTeam = scanner2.next();
             int lossScore = scanner2.nextInt();
             char loc = scanner2.next().charAt(0);
+            scanner2.close();
             Team w;
             Team l;
-            if (!teamIDs.contains(winningTeam)) {
+            /*if (!teamIDs.contains(winningTeam)) {
                 w = new Team(winningTeam);
                 teams.add(w);
                 w.pointsFor += winScore;
                 w.pointsAgainst += lossScore;
-            } else {
+            } else {*/
                 w = teams.get(teamIDs.indexOf(winningTeam));
                 w.pointsFor += winScore;
                 w.pointsAgainst += lossScore;
-            }
-            if (!teamIDs.contains(losingTeam)) {
+            //}
+            /*if (!teamIDs.contains(losingTeam)) {
                 l = new Team(losingTeam);
                 teams.add(l);
                 l.pointsFor += lossScore;
                 l.pointsAgainst += winScore;
-            } else {
+            } else {*/
                 l = teams.get(teamIDs.indexOf(losingTeam));
                 l.pointsFor += lossScore;
                 l.pointsAgainst += winScore;
-            }
+            //}
             Elo.applyEloChange(w, winScore, l, lossScore, loc);
         }
+        scanner.close();
     }
 }
--- src/com/company/Team.java
+++ src/com/company/Team.java
@@ -9,13 +9,15 @@ public class Team {
     public int games;
     public String id;
     public int elo;
+    public String name;
 
-    public Team(String idVal) {
+    public Team(String teamName, String idVal) {
         pointsFor = 0;
         pointsAgainst = 0;
         games = 0;
         id = idVal;
         elo = 1500;
+        name = teamName;
     }
 
     public double pointsPercentage() {
