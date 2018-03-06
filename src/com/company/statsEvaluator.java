480a8df9fad9861574c05aa247700d03f31c7167
jd997 <john.m.durrell@gmail.com>
2018-03-06 08:45:40 -0500

    bracket-prediction

--- src/com/company/statsEvaluator.java
+++ src/com/company/statsEvaluator.java
@@ -8,15 +8,15 @@ import java.util.DoubleSummaryStatistics;
  */
 public class statsEvaluator {
 
-    public static double pythagoreanPercentageCalc(int pointsFor, int pointsAgainst, int gamesPlayed) {
-        return gamesPlayed*(Math.pow(pointsFor, 16.5)/(Math.pow(pointsFor, 16.5) + Math.pow(pointsAgainst, 16.5)));
+    public static double pythagoreanPercentageCalc(Team team) {
+        return team.games*(Math.pow(team.pointsFor, 16.5)/(Math.pow(team.pointsFor, 16.5) + Math.pow(team.pointsAgainst, 16.5)));
     }
 
     public static double gameScoreCalc(int points, int fieldGoals, int FGA, int FT, int FTA, int ORB, int DRB, int steals, int assists, int blocks, int personalFouls, int turnovers) {
         return points +(0.4*fieldGoals)- (0.7*FGA) - (0.4*(FTA - FT)) + (0.7 * ORB) + (0.3 * DRB) + steals + (0.7 * assists) + (0.7 * blocks) - (0.4 * personalFouls) - turnovers;
     }
 
-    public static double fantasyBasketball(int WFGM, int WFGM3, int WFTM, int WOR, int WDR, int WAst, int WStl, int WBlk, int WTO){
+    public static double fantasyBasketball(int WFGM, int WFGM3, int WFTM, int WOR, int WDR, int WAst, int WStl, int WBlk, int WTO) {
         double sum = (WFGM * 2) + (WFGM3 * 3) + WFTM + ((WOR + WDR) * 1.2) + (WAst * 1.5) + (WStl * 3) + (WBlk * 3) + (WTO * -1);
         return sum;
     }
@@ -26,9 +26,9 @@ public class statsEvaluator {
     2 == fantasy basketball
      */
 
-    public static void ranking(int calctype, Team x){
+    public static void ranking(int calctype, Team x) {
         if (calctype ==  0) {
-            x.scoreOfEvluator = pythagoreanPercentageCalc(x.pointsFor, x.pointsAgainst, x.games);
+            x.scoreOfEvluator = pythagoreanPercentageCalc(x);
         }
         else if (calctype == 1) {
             //  x.scoreOfEvluator = gameScoreCalc()
@@ -41,9 +41,18 @@ public class statsEvaluator {
 
     }
 
-    public static void rankPrint(){
+    public static void rankPrint() {
         for (int x = 0; x < Main.teams.size(); x++) {
             System.out.println(x+1 + ": " + Main.teams.get(x).name + " - " + Main.teams.get(x).elo);
+        }
+    }
+
+    public static int prediction(Team team1, Team team2) {
+        double eloDiff = Math.abs(team1.elo-team2.elo);
+        if ((Math.pow(Math.E, (pythagoreanPercentageCalc(team1)*eloDiff))/(1+Math.pow(Math.E, (pythagoreanPercentageCalc(team1)*eloDiff)))) > .5) {
+            return 1;
+        } else {
+            return 0;
         }
     }
 
